937d04c4e04f44dc88c72950cfb778ac5290be59
armel <armelzong@gmail.com>
2020-05-04 15:18:29 +0000

    making_a_todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
+// MODULE IMPORTED FROM ANGULAR
+
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// IMPORTING THE MAIN COMPONENT AND THE OTHER COMPONENTS
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
+
 import { DatasService } from './services/datas.service';
+import { TodoComponent } from './components/todo/todo/todo.component';
+import { TableComponent } from './components/table/table/table.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    TodoComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
